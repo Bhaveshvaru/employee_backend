733a3b937dc8076eaa8f115c93e2a6b7e17e3a34
bhaveshvaru <bhavesh.varu97@gmail.com>
2020-11-19 18:15:38 +0530

    added excel controller and route

--- controller/employee.js
+++ controller/employee.js
@@ -4,6 +4,8 @@ const _ = require("lodash");
 const fs = require("fs");
 const path = require('path');
 const shortid = require('shortid');
+const ExcelJs = require('exceljs');
+
 
 //create Employee controller
 exports.createEmployee=(req,res)=>{
@@ -19,13 +21,15 @@ exports.createEmployee=(req,res)=>{
   }
 
   const file = req.files.photo
-  
+
+  //image validation 
   if(file.mimetype !== 'image/jpeg')
   {
     res.status(400).json({message:"Please upload proper image!"});
     return;
   }
-  let shortId=shortid.generate()
+  let shortId=shortid.generate();
+
   file.name = `photo_${shortId}_${file.name}`;
 
   file.mv(`uploads/${file.name}`, async err => {
@@ -43,11 +47,12 @@ exports.createEmployee=(req,res)=>{
  
     employee.save((error, data) => {
 
-      //custom Error Handling
+     // custom Error Handling
       if(error !== null){
         if(error.code === 11000){
           return res.status(400).json({message:"Email already Exists!"});
         }
+       
         if( error.errors.phone && error.errors.phone.reason.code === "ERR_ASSERTION"){
          return res.status(400).json({message:"Please Input number in Phone Field."});
        }
@@ -115,33 +120,34 @@ exports.deleteEmployee =(req,res,next)=>{
 
 //update employee Controller
 exports.updateEmployee=(req,res,next)=>{
-  const id= req.params.id;
-  const file = req.files.photo
-  if(!req.files)
-  {
-      res.status(400).json({msg:"Image was not found"});
-      return;
-  }
-  
-  let shortId=shortid.generate()
-  file.name = `photo_${shortId}_${file.name}`;
-
-  file.mv(`uploads/${file.name}`, async err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({err:"problem with image"})
+    const id= req.params.id;
+    const file = req.files.photo
+    if(!req.files)
+    {
+        res.status(400).json({msg:"Image was not found"});
+        return;
     }
-  })
     
-  let photo=file.name;//.file
-  const reqd=req.body;
-  const reqData= {reqd,photo};
-  Employee.findByIdAndUpdate(id,reqData,{new:true})
-  .exec((error,data)=>{
-     if (error) return res.status(400).json({ error });
-     if(data)  res.status(201).json({ message:"employee updated successfully!",data,
-    }); 
-  })
+    let shortId=shortid.generate()
+    file.name = `photo_${shortId}_${file.name}`;
+
+    file.mv(`uploads/${file.name}`, async err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({err:"problem with image"})
+      }
+    })
+      
+    let photo=file.name;//.file
+    const reqd=req.body;
+    const reqData= {reqd,photo};
+    Employee.findByIdAndUpdate(id,reqData,{new:true})
+    .exec((error,data)=>{
+      if (error) return res.status(400).json({ error });
+      if(data)  res.status(201).json({ message:"employee updated successfully!",data,
+      }); 
+    })
+
 }
 
 
@@ -153,9 +159,44 @@ exports.updateEmployee=(req,res,next)=>{
     }
     Employee.find({employeename:{$regex:searchField,$options:'$i'}})
     .then(data=>{
-      return res.status(200).json(data)
+      if(data){
+        return res.status(200).json(data);
+      };
+      
     })
     .catch(err=>{
       return res.status(400).json({error:err})
     })
   }
+
+  //get employee excel sheet 
+   exports.getEmployeeExcelData= async(req,res,next)=>{
+    try {
+      const employeeData = await Employee.find({});
+      const workbook = new ExcelJs.Workbook();
+      const worksheet = workbook.addWorksheet('Employee');
+      worksheet.columns=[
+        {header:'S no',key:'s_no',width:10 },
+        {header:'Employee Name',key:'employeename',width:10 },
+        {header:'Employee Address',key:'employeeaddress',width:10 },
+        {header:'Email',key:'email',width:10 },
+        {header:'Date Of Birth',key:'dateofbirth',width:10 },
+        {header:'Phone Number',key:'phone',width:10 },
+        {header:'Employee Bio',key:'bio',width:10 },
+      ]
+      let count =1;
+      employeeData.forEach(user=>{
+        user.s_no = count;
+        worksheet.addRow(user);
+        count+=1;
+      });
+      worksheet.getRow(1).eachCell((cell)=>{
+        cell.font = {bold:true}
+      });
+      const data = await workbook.xlsx.writeFile('./uploads/Employee.xlsx')
+      res.status(201).json({message:"Employee Excel sheet Created!"})
+
+    } catch (error) {
+     res.status(500).json({error}) 
+    }
+   }
--- route/employee.js
+++ route/employee.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllemployee,createEmployee,getEmployeeById,deleteEmployee,updateEmployee,getEmployeeByName}= require("../controller/employee");
+const {getAllemployee,createEmployee,getEmployeeById,deleteEmployee,updateEmployee,getEmployeeByName,getEmployeeExcelData}= require("../controller/employee");
 
 router.post("/employee/create",createEmployee);
 router.get("/employee/get",getAllemployee);
@@ -9,5 +9,8 @@ router.get("/employeeId/:id",getEmployeeById);
 router.delete("/employee/delete/:id",deleteEmployee);
 router.put("/employee/update/:id",updateEmployee);
 router.get("/employee/search",getEmployeeByName)
+router.get("/employee/excelData",getEmployeeExcelData)
+
+//getEmployeeExcelData
 
 module.exports=router;
